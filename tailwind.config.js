b5481aa4d934173fce44ba32b1c5a00db4577fbb
Damon Bakker <damon@devmon.nl>
2024-01-29 18:05:30 +0100

    Services development

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,17 @@
 export default {
   content: [],
   theme: {
-    extend: {},
-    colors: {
-      'dark-theme-bg-color' : '#0f172a',
-      'dark-theme-white': 'rgba(229,231,235,0.89)',
-      'white': '#FFFFFF',
-      'dark-theme-header-color': '#01515a',
-      'theme-btn': 'rgb(6,140,201)'
+    extend: {
+      colors: {
+        'dark-theme-bg-color': '#0f172a',
+        'dark-theme-white': 'rgba(229,231,235,0.89)',
+        'white': '#FFFFFF',
+        'dark-theme-header-color': '#01515a',
+        'theme-btn': 'rgb(6,140,201)',
+        'slate-500': 'rgb(100 116 139)'
+      },
     },
+
   },
   plugins: [],
 }
